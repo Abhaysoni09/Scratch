168ccc7746a43d7529ea5f37bfb734931ffc400f
Abhaysoni096 <abhaylkw332@gmail.com>
2026-06-30 06:43:21 +0530

    API changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,9 @@ const app = express()
 app.use(express.json())
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173",
+            "https://scratch-wyu3.onrender.com"
+        ],
         credentials: true,
     }
 ))
